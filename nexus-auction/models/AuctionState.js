317d2cbdf914644ad5117ad989d2204dda25d1d0
Anu-Codex <sarkaranubhav55@gmail.com>
2026-04-26 20:55:57 +0530

    Update AuctionState.js

--- nexus-auction/models/AuctionState.js
+++ nexus-auction/models/AuctionState.js
@@ -1,1 +1,9 @@
+const mongoose = require('mongoose');
 
+const auctionStateSchema = new mongoose.Schema({
+    activePlayerId: { type: mongoose.Schema.Types.ObjectId, ref: 'Player', default: null },
+    currentBid: { type: Number, default: 0 },
+    highestBidder: { type: String, default: null }
+});
+
+module.exports = mongoose.model('AuctionState', auctionStateSchema);
