33a505fadc4f37cfa1e81405a8f20869e042bab8
Anu-Codex <sarkaranubhav55@gmail.com>
2026-04-26 20:55:26 +0530

    Update Player.js

--- nexus-auction/models/Player.js
+++ nexus-auction/models/Player.js
@@ -1,1 +1,12 @@
+const mongoose = require('mongoose');
 
+const playerSchema = new mongoose.Schema({
+    name: { type: String, required: true },
+    strength: { type: Number, required: true },
+    cardType: { type: String, required: true },
+    value: { type: Number, required: true },
+    status: { type: String, default: 'Available' }, // 'Available' or 'Sold'
+    soldTo: { type: String, default: '-' }
+});
+
+module.exports = mongoose.model('Player', playerSchema);
